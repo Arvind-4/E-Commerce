0bdfb2d84d66781105ed4adae60db474b6fc8932
Arvind-4 <lordarvindthegamer@gmail.com>
2022-01-22 15:26:30 +0530

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import type { Component } from 'solid-js'
 import { Route, Routes } from 'solid-app-router'
 
 import Homepage from './pages/Homepage'
-import Aboutpage from './pages/Aboutpage'
 import ProductListpage from './pages/ProductListpage'
 import ProductDetailpage from './pages/ProductDetailpage'
 import Categorypage from './pages/Categorypage'
+import Contactpage from './pages/Contactpage'
+import Aboutpage from './pages/Aboutpage'
+import CategoryDetailpage from './pages/CategoryDetailpage'
 import Errorpage from './pages/Errorpage'
 
 import Header from './snippits/Header'
@@ -16,10 +18,12 @@ const App: Component = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/about/' element={<Aboutpage />} />
         <Route path='/products/' element={<ProductListpage />} />
         <Route path='/categories/' element={<Categorypage />} />
+        <Route path='/contact-us/' element={<Contactpage />} />
+        <Route path='/about-us/' element={<Aboutpage />} />
 
+        <Route path='/category/:slug/' element={<CategoryDetailpage />} />
         <Route path='/:id/detail-view/:slug/' element={<ProductDetailpage />} />
 
       </Routes>
