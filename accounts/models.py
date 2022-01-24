638c03f96434d9ca79c8349d5c2e36e079cc5341
Arvind-4 <lordarvindthegamer@gmail.com>
2022-01-24 18:26:41 +0530

    Added forget password & change password

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, 
-    BaseUserManager
+    BaseUserManager,
 )
 
 class MyAccountManager(BaseUserManager):
@@ -12,7 +12,6 @@ class MyAccountManager(BaseUserManager):
         user  = self.model(
                 email=self.normalize_email(email),
             )
-
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -36,13 +35,12 @@ class Account(AbstractBaseUser):
         editable=False, 
         unique=True
     )
-    email      = models.EmailField(verbose_name='email', max_length=60, unique=True)
-    username     = models.CharField(max_length=30,unique=True)
-    date_joined  = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login   = models.DateTimeField(verbose_name="last login", auto_now=True)
-    is_admin     = models.BooleanField(default=False)
-    is_active    = models.BooleanField(default=True)
-    is_staff     = models.BooleanField(default=False)
+    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
