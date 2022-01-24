729befd0626883be088f3b5c71bcd891f4d46cfe
Arvind-4 <lordarvindthegamer@gmail.com>
2022-01-24 18:31:45 +0530

    Changed command

--- commands/bundle-frontend.sh
+++ commands/bundle-frontend.sh
@@ -1,4 +1,7 @@
 rm -rf dist/ 
 yarn build:production
 cd static-dev/css-dev/
-yarn production
+yarn build:production
+cd ..
+cd ..
+rm -rf static && python manage.py collectstatic -i css-dev -i admin -i rest_framework --no-input
