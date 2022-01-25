1c614d241bc5a4a7459ad629d2c6d211b07faadc
Arvind-4 <lordarvindthegamer@gmail.com>
2022-01-25 14:23:08 +0530

    Modified App.tsx for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
 import type { Component } from 'solid-js'
-import { Route, Routes } from 'solid-app-router'
+import { useLocation, useRoutes } from 'solid-app-router'
 
-import Homepage from './pages/Homepage'
-import ProductListpage from './pages/ProductListpage'
-import ProductDetailpage from './pages/ProductDetailpage'
-import Categorypage from './pages/Categorypage'
-import Contactpage from './pages/Contactpage'
-import Aboutpage from './pages/Aboutpage'
-import CategoryDetailpage from './pages/CategoryDetailpage'
-import Errorpage from './pages/Errorpage'
-
+import { routes } from './router'
 import Header from './snippits/Header'
 
 const App: Component = () => {
+  const location = useLocation()
+  const exists = routes.some(route => route.path === location.pathname);
+  if (exists === false) {
+    return window.location.href = '/error/'
+  }
+  const URL = useRoutes(routes)
   return (
     <>
       <Header />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/products/' element={<ProductListpage />} />
-        <Route path='/categories/' element={<Categorypage />} />
-        <Route path='/contact-us/' element={<Contactpage />} />
-        <Route path='/about-us/' element={<Aboutpage />} />
-
-        <Route path='/category/:slug/' element={<CategoryDetailpage />} />
-        <Route path='/:id/detail-view/:slug/' element={<ProductDetailpage />} />
-
-      </Routes>
+      <URL />
     </>
   )
 }
