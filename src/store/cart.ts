f6ab970ef4529a6be5730045d60e20818a3934e0
Arvind-4 <lordarvindthegamer@gmail.com>
2022-01-31 22:00:59 +0530

    Made Changes to base.html

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -6,13 +6,13 @@ import { CartInterface, ProductInterface } from "../interface/product"
 import { is_authenticated } from "./auth"
 
 export async function fetchUserCart() {
-    if (is_authenticated === 'true') {
-        const res = await fetch(`${base_url}/api/cart/`)
-        const data = await res.json()
-        return data
-    } else {
-        return []
-    }
+  if (is_authenticated === 'true') {
+    const res = await fetch(`${base_url}/api/cart/`)
+    const data = await res.json()
+    return data
+  } else {
+    return []
+  }
 }
 
 export function SuccessAlert(title: string) {
@@ -22,15 +22,15 @@ export function SuccessAlert(title: string) {
     showConfirmButton: false,
     timer: 3000,
     timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
+    // didOpen: (toast) => {
+    //     toast.addEventListener('mouseenter', Swal.stopTimer)
+    //     toast.addEventListener('mouseleave', Swal.resumeTimer)
+    // }
   })
   Toast.fire({
     icon: 'success',
     title: `${title} Added to Your Cart`
-  })  
+  })
 }
 
 export function DeleteAlert(title: string) {
@@ -42,67 +42,67 @@ export function DeleteAlert(title: string) {
 }
 
 export const [cartproducts, { refetch }] = createResource<CartInterface[]>(
-    () => fetchUserCart(),
-    {
-        initialValue: []
-    }
+  () => fetchUserCart(),
+  {
+    initialValue: []
+  }
 )
 
 export const dataRefetch = function () {
-    refetch()
-    return null
+  refetch()
+  return null
 }
 
 export const cart = createMutable({
-    addProduct({ product }: { product: ProductInterface }) {
-        try {
-            const response = fetch(`${base_url}/api/cart/add/${product.slug}/${product.id}/`)
-            return true
-        } catch {
-            return false
+  addProduct({ product }: { product: ProductInterface }) {
+    try {
+      const response = fetch(`${base_url}/api/cart/add/${product.slug}/${product.id}/`)
+      return true
+    } catch {
+      return false
 
-        }
-    },
-    removeProduct({ product }: { product: ProductInterface }) {
-        try {
-            const response = fetch(`${base_url}/api/cart/remove/${product.slug}/${product.id}/`)
-            return true
-        } catch {
-            return false
-
-        }
     }
+  },
+  removeProduct({ product }: { product: ProductInterface }) {
+    try {
+      const response = fetch(`${base_url}/api/cart/remove/${product.slug}/${product.id}/`)
+      return true
+    } catch {
+      return false
+
+    }
+  }
 })
 
 export async function deleteProduct(product: ProductInterface) {
-    if (is_authenticated === 'true') {
-        const value = cart.removeProduct({ product })
-        if (value) {
-            DeleteAlert(product.title)
-            dataRefetch()
-        } else {
-            alert(`Failed`)
-        }
+  if (is_authenticated === 'true') {
+    const value = cart.removeProduct({ product })
+    if (value) {
+      DeleteAlert(product.title)
+      dataRefetch()
     } else {
-        var url = window.location.pathname
-        var login_redirect = `/accounts/sign-in/?next=${url}/`
-        window.location.href = url.replace(url, login_redirect)
+      alert(`Failed`)
     }
+  } else {
+    var url = window.location.pathname
+    var login_redirect = `/accounts/sign-in/?next=${url}/`
+    window.location.href = url.replace(url, login_redirect)
+  }
 }
 
 export async function navigateUser(product: ProductInterface) {
-    if (is_authenticated === 'true') {
-        const value = cart.addProduct({ product })
-        if (value) {
-            SuccessAlert(product.title)
-            dataRefetch()
-        } else {
-            alert(`Failed`)
-        }
+  if (is_authenticated === 'true') {
+    const value = cart.addProduct({ product })
+    if (value) {
+      SuccessAlert(product.title)
+      dataRefetch()
     } else {
-        var url = window.location.pathname
-        var login_redirect = `/accounts/sign-in/?next=${url}/`
-        window.location.href = url.replace(url, login_redirect)
+      alert(`Failed`)
     }
+  } else {
+    var url = window.location.pathname
+    var login_redirect = `/accounts/sign-in/?next=${url}/`
+    window.location.href = url.replace(url, login_redirect)
+  }
 
 }
