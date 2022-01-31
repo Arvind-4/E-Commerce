0e006399b93b64644bf6a7e9bf03cb0009be0d22
Arvind-4 <lordarvindthegamer@gmail.com>
2022-01-31 21:59:24 +0530

    New Page Accounts

---
+++ src/pages/Accountspage.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+import { cartproducts } from "../store/cart";
+
+const Accountpage: Component = () => {
+    return <div>Account Page</div>;
+};
+
+export default Accountpage;
