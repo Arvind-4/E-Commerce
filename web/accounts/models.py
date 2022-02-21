f27234b303f4328a0a8ad6f0f7a849a00a3cd4dd
Arvind-4 <lordarvindthegamer@gmail.com>
2022-02-21 21:18:23 +0530

    Removed verbose field

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -35,9 +35,9 @@ class Account(AbstractBaseUser):
         editable=False, 
         unique=True
     )
-    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
-    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+    email = models.EmailField(max_length=60, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
