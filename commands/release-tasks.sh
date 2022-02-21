03c0a943043dc7f81c4d75e215561e2699e655ea
Arvind-4 <lordarvindthegamer@gmail.com>
2022-02-21 21:41:54 +0530

    Fixed migration error

--- commands/release-tasks.sh
+++ commands/release-tasks.sh
@@ -1,4 +1,4 @@
-python web/manage.py makemigration --noinput
+python web/manage.py makemigrations --noinput
 python web/manage.py migrate --noinput
 
 python web/manage.py collectstatic -i rest_framework -i flags --no-input
