33d7aad1aadb57eacd4330daca85b2c66b5e45fd
Arvind-4 <lordarvindthegamer@gmail.com>
2022-05-06 18:24:29 +0530

    Updated: Release Commands for Algolia-Search

--- commands/release-tasks.sh
+++ commands/release-tasks.sh
@@ -1,2 +1,11 @@
+# DB Release Tasks
+
 python web/manage.py makemigrations --noinput
 python web/manage.py migrate --noinput
+
+
+# Alogia Release Tasks
+
+python web/manage.py algolia_applysettings
+python web/manage.py algolia_clearindex
+python web/manage.py algolia_reindex
