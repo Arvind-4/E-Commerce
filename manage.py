c9e16b4e4dd60a72dcb6c2e8ef5d7f545e01fb8a
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-19 13:43:36 +0530

    Added: moved manage.py to root

---
+++ manage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+PATHS = [
+    str(BASE_DIR / 'apps'),
+    str(BASE_DIR / 'apps' / 'web'),
+    str(BASE_DIR / 'apps' / 'web' / 'backend'),
+]
+
+sys.path.extend(PATHS)
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
