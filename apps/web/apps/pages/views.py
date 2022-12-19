038b15b50bc6cf8d22c2dd64cefedae0331e3f2b
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-19 16:23:42 +0530

    Added: 404 and 500 views

--- apps/web/apps/pages/views.py
+++ apps/web/apps/pages/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.views.generic import View
 
 # Create your views here.
 
-def error_404(request, exception=404):
-    return render(request,'error/404.html', context={})
+def handler404(request:HttpRequest, *args, **kwargs):
+    return render(request, 'error/404.html', status=404, context={})
 
-def error_500(request,  exception):
-        context = {}
-        return render(request,'error/500.html', context=context)
+def handler500(request:HttpRequest, *args, **kwargs):
+    return render(request, 'error/500.html', status=500, context={})
+
+class ReactView(View):
+    template_name = "base.html"
+    context = {}
+    def get(self, request:HttpRequest, *args, **kwargs):
+        return render(request, self.template_name, self.context)
